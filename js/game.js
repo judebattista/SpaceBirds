ebaeb70bac84cbabc53582016033f686682271a2
Jude M Battista <jbattista20@my.whitworth.edu>
2019-01-11 13:12:56 -0800

    Procedurally generate astroid divs and animate.
    Currently rocks are generated at the start of the level and move right
    to left across the screen at varying speeds.
    
    Note: Rocks are represented by red boxes for ease of debugging.
    
    TODO: 	Generate rocks as the level scrolls
    	Add rock image to div
    	Randomize rock image?
    	Add collision detection
    		- radial based on center of div?

---
+++ js/game.js
@@ -0,0 +1,26 @@
+var gameState =
+{
+    level:10,
+};
+
+$(document).ready(
+    function () {
+        //The number of rocks is determined by the level
+        var numRocks = gameState.level;
+        var $rockMom =  $("#gameBox");
+        var $rock;
+        var portHeight = $rockMom.height();
+        var rockVerticalGap = portHeight / numRocks;
+        for (var ndx = 0; ndx < numRocks; ndx++) {
+            $rock = $("<div>");
+            $rock.attr("id", "rock" + ndx);
+            $rock.attr("class", "rock");
+            $rock.css("top", ndx*rockVerticalGap);
+            $rock.css("animation-duration", Math.random() * 10 + 1 + "s");
+            $rockMom.append($rock);
+        }
+    }
+);
+
+
+
