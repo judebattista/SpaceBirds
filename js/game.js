72a4d4d0bbd912d4dc9716a118e51228268e1784
Alyssa C Fusato <afusato21@my.whitworth.edu>
2019-01-13 20:51:27 -0800

    Add splash screens
    Add screens to allow the user to start the game or reset it after a win
    or loss.
    
    Handle element resets or removals on game reset
    
    The menu elements themselves do not reset properly. They should reuse
    elements instead of being removed each time.
    
    The asteroid frequency does not currently reset properly on game reset.
    Instead, the asteroids become cumulative which is fine if the player
    wins but somewhat obnoxious if they just lost.
    
    The bird periodically fails to reset its position on game reset. No idea
    why.
    
    Probably some other stuff too.

--- js/game.js
+++ js/game.js
@@ -4,14 +4,6 @@ var rockRadius = 25;
 var rockDamage = 1;
 var baseFontSize = $("body").css("font-size");
 
-var obstacle = {
-    radius: 0,
-    centerx: 0,
-    centery: 0,
-    speed: 0,
-    active: 0,
-};
-
 var nest = { /* added from nest.js */
     radius: 10,
     speed: 0,
@@ -22,8 +14,9 @@ var gameState =
 {
     level: 5,
     timer: 0, /*loop iterations*/
+    active: false,
     activeObst: 0,
-    activeNest:0, /* added from nest.js */
+    activeNest: 0, /* added from nest.js */
     levelEnd: 105,
 };
 
@@ -39,49 +32,115 @@ var bird = {
 
 $(document).ready(
     function () {
-        var runLoop = setInterval(gameLoop, loopDelay);
-        //make bird element
-        var $birdMom = $("#gameBox");
-        $bird = $("<div>");
-        $bird.attr("id", "bird");
-        $bird.attr("class", "bird");
-        $bird.css("top", $birdMom.position().top);
-        $bird.css("left", $birdMom.position().left);
-        $birdMom.append($bird);
-
-
-        //Set static bird information
-        bird.height = $bird.height();
-        bird.width = $bird.width();
-        bird.radius = Math.min(bird.height, bird.width) / 2;
-
-        //Add the galaxy backdrop
-        var $backgroundMom = $("#gameBox");
-        var $galaxyGraphics = $("<div>");
-        $galaxyGraphics.attr("class", "sliding-galaxies");
-        $galaxyGraphics.appendTo($backgroundMom);
+        var startBtn = "Start";
+        var startText = "<div class=\"splashtext\">Use the arrow keys to steer the bird away from asteroids. Reach the bird's space ship to win!</div>";
+        gamescreen(startBtn, startText);
     }
 );
 
+function gamescreen(buttontext, splashtext) {
+    var $button = $("<div>");
+    $button.attr("id", "startbutton");
+    $button.attr("class", "startbutton");
+    $button.html(buttontext);
+    $button.on("click", function () {
+        gameReset();
+        $button.parent().css("display", "none");
+        gameRun();
+        $button.parent().remove();
+    });
+    /*var $img = $("<div>");*/
+    var $startscreen = $("<div>");
+    $startscreen.attr("id", "splashscreen");
+    $startscreen.attr("class", "splashscreen");
+    $startscreen.html(splashtext);
+    $startscreen.append($button);
+    $startscreen.appendTo($("#gameBox"));
+}
+
+function gameReset() {
+    var $nest = $("#nest");
+    if ($nest) {
+        $nest.css("display", "none");
+        $nest.remove();
+    }
+    var $bird = $("#bird");
+    if ($bird) {
+        $bird.css("display", "none");
+    }
+    var $rock = $(".rock");
+    if ($rock.length > 0) {
+        $rock.each(function () {
+            $(this).css("display", "none");
+            $(this).remove();
+        });
+    }
+    bird.healthCurrent = bird.healthMax;
+    gameState.timer = 0;
+}
+
+function gameRun() {
+    var runLoop = setInterval(gameLoop, loopDelay);
+    //make bird element
+    var $birdMom = $("#gameBox");
+    var $bird = $("#bird");
+    if ($bird.length == 0) {
+        $bird = $("<div>");
+        $bird.attr("id", "bird");
+        $bird.attr("class", "bird");
+        $birdMom.append($bird);
+    }
+
+    $bird.css("top", $birdMom.position().top);
+    $bird.css("left", $birdMom.position().left);
+    $bird.css("display", "inline");
+    //Set static bird information
+    bird.height = $bird.height();
+    bird.width = $bird.width();
+    bird.radius = Math.min(bird.height, bird.width) / 2;
+
+    //Add the galaxy backdrop
+    var $backgroundMom = $("#gameBox");
+    var $galaxyGraphics = $(".sliding-galaxies");
+    if ($galaxyGraphics.length == 0) {
+        $galaxyGraphics = $("<div>");
+        $galaxyGraphics.attr("id", "sliding-galaxies");
+        $galaxyGraphics.attr("class", "sliding-galaxies");
+
+        $galaxyGraphics.appendTo($backgroundMom);
+    }
+    gameState.active = true;
+}
+
+function gameLose() {
+    gameState.active = false;
+    gameReset();
+    var startBtn = "Retry";
+    var startText = "<div class=\"splashtext\">You died :(</div>";
+    gamescreen(startBtn, startText);
+}
+
 function gameLoop() {
-    //console.log("Game loop executing");
-    generateAsteroids();
-    generateNest(); /* added from nest.js */
-    destroyInactiveAsteroids();
-    detectCollision();
-    updateBird();
-    gameState.timer++;
+    if (gameState.active) {
+        //console.log("Game loop executing");
+        generateAsteroids();
+        generateNest(); /* added from nest.js */
+        destroyInactiveAsteroids();
+        detectCollision();
+        updateBird();
+        gameState.timer++;
+    }
 }
 
 function generateNest() { /* added from nest.js */
     if (gameState.timer == gameState.levelEnd) {
-        
+
         var $nestMom = $("#gameBox");
         var $nest;
         $nest = $("<div>");
         $nest.attr("id", "nest");
         $nest.attr("class", "nest");
-        $nest.css("top",  $nestMom.position().top + 5);
+        $nest.css("top", $nestMom.position().top + 5);
         $nestMom.append($nest);
     }
 }
@@ -100,7 +159,7 @@ function generateAsteroids() {
             $rock.attr("id", "rock" + ndx);
             $rock.attr("class", "rock");
             $rock.css("top", ndx * rockVerticalGap + portTop);
-            $rock.css("animation-duration",  Math.random() * 5 + 1 + "s");
+            $rock.css("animation-duration", Math.random() * 5 + 1 + "s");
             $rockMom.append($rock);
         }
     }
@@ -109,7 +168,7 @@ function generateAsteroids() {
 function detectCollision() {
     //console.log("Detecting collisions");
     $(".rock").each(doesRockOverlapBird);
-    if (gameState.timer >= gameState.levelEnd){
+    if (gameState.timer >= gameState.levelEnd) {
         hasBirdReachedNest();
     }
 }
@@ -144,21 +203,25 @@ function hasBirdReachedNest() {
     var $nest = $("#nest");
     var nestX = $nest.position().left;
     var nestWidth = $nest.width();
-    var nestCenter = nestX + (nestWidth/2);
+    var nestCenter = nestX + (nestWidth / 2);
 
     var $bird = $("#bird");
     var birdX = $bird.position().left;
     var birdWidth = $bird.width();
-    var  birdCenter =  birdX + ( birdWidth/2);
+    var birdCenter = birdX + (birdWidth / 2);
 
-    if(birdCenter >= nestCenter){
+    if (birdCenter >= nestCenter) {
         /* win */
-        alert("Victory!!");
+        gameState.active = false;
+        gameReset();
+        var startBtn = "Play again (with more asteroids)";
+        var startText = "<div class=\"splashtext\"> You didn't died :) </div>";
+        gamescreen(startBtn, startText);
     }
 }
 
 function destroyInactiveAsteroids() {
-    $(".rock").each( function() {
+    $(".rock").each(function () {
         $this = $(this);
         var rockPos = $this.position();
         var rockLeft = rockPos.left;
@@ -169,10 +232,9 @@ function destroyInactiveAsteroids() {
 }
 
 function updateBird() {
-    if (bird.healthCurrent <= 0){
-        alert("death");
+    if (bird.healthCurrent <= 0) {
+        gameLose();
     }
-    var $bird = $("#bird");
 }
 
 function overlapRadial(x1, y1, x2, y2, radius1, radius2) {
@@ -183,15 +245,15 @@ function overlapRadial(x1, y1, x2, y2, radius1, radius2) {
     var distance = Math.sqrt(xdistSquared + ydistSquared);
     var sumRad = radius1 + radius2;
     //console.log("y1=" + y1 + " y2=" + y2 + " xdist=" + xdist + " ydist=" + ydist + " xSquared=" + xdistSquared + " ySquared=" + ydistSquared + " calculated distance: " + distance + " radius " + sumRad);
-    
+
     if (distance < sumRad) {
         console.log("COLLISION! Distance: " + distance + " radius: " + sumRad);
     }
-    
+
     return distance < sumRad;
 }
 
-function overlapBox ($bird, $rock ) {
+function overlapBox($bird, $rock) {
     //Check to see if 1 hit the bottom of 2
     birdTop = $bird.position().top;
     birdBottom = birdTop + $bird.height();
@@ -204,9 +266,9 @@ function overlapBox ($bird, $rock ) {
     rockRight = rockLeft + $rock.width();
 
     var collision = true;
-    if ((birdBottom < rockTop) || 
-        (birdTop > rockBottom) || 
-        (birdLeft > rockRight) || 
+    if ((birdBottom < rockTop) ||
+        (birdTop > rockBottom) ||
+        (birdLeft > rockRight) ||
         (birdLeft < rockLeft)) {
         collision = false;
     } else {
@@ -215,14 +277,14 @@ function overlapBox ($bird, $rock ) {
     return collision;
 }
 
-function hitBird (damage){
+function hitBird(damage) {
     bird.healthCurrent -= damage;
     var $bird = $("#bird");
-    $bird.css("animation-name",  "birdHit");
-    window.setTimeout(function(){
-        $bird.css("animation-name",  "none");
+    $bird.css("animation-name", "birdHit");
+    window.setTimeout(function () {
+        $bird.css("animation-name", "none");
     }, 1000);
-    
 }
+
 
 
