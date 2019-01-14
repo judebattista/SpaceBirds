60861e5bf1fd2010f3bec2940efff242d3edce0c
Alyssa C Fusato <afusato21@my.whitworth.edu>
2019-01-13 17:11:23 -0800

    Bird stays in gamebox
    Changed keyboard input so bird cannot go past the bounds of the gamebox.
    Before the bird moves, the distance from the boundary is checked and if
    the bird is close to the border, it is only allowed to move up to the
    boundary instead of past it.

--- js/movebird.js
+++ js/movebird.js
@@ -4,29 +4,95 @@ https://www.tutorialrepublic.com/codelab.php?topic=faq&file=jquery-move-an-eleme
 https://jeremyckahn.github.io/keydrown/
 
 */
+var moveDist = 9;
 
 kd.DOWN.down = function () {
-    $(".bird").finish().animate({
-        top: "+=9"
-    });
+    var $bird = $("#bird");
+    var birdTop = $bird.position().top;
+    var birdHeight = $bird.height();
+    var birdBottom = birdTop + birdHeight;
+
+    var $gameBox = $("#gameBox");
+    var gameBoxTop = $gameBox.position().top;
+    var gameBoxHeight = $gameBox.height();
+    var gameBoxBottom = gameBoxTop + gameBoxHeight;
+    var distance = gameBoxBottom - birdBottom;
+
+    if (distance < moveDist) {
+        $(".bird").finish().animate({
+            top: "+=" + distance
+        });
+    }
+    else {
+        $(".bird").finish().animate({
+            top: "+=" + moveDist
+        });
+    }
 };
 
 kd.UP.down = function () {
-    $(".bird").finish().animate({
-        top: "-=9"
-    });
+    var $bird = $("#bird");
+    var birdTop = $bird.position().top;
+
+    var $gameBox = $("#gameBox");
+    var gameBoxTop = $gameBox.position().top;
+
+    var distance = birdTop - gameBoxTop;
+
+    if (distance < moveDist) {
+        $(".bird").finish().animate({
+            top: "-=" + distance
+        });
+    }
+    else {
+        $(".bird").finish().animate({
+            top: "-=" + moveDist
+        });
+    }
 };
 
 kd.LEFT.down = function () {
-    $(".bird").finish().animate({
-        left: "-=9"
-    });
+    var $bird = $("#bird");
+    var birdLeft = $bird.position().left;
+
+    var $gameBox = $("#gameBox");
+    var gameBoxLeft = $gameBox.position().left;
+    var distance = birdLeft - gameBoxLeft;
+    
+    if (distance < moveDist) {
+        $(".bird").finish().animate({
+            left: "-=" + distance
+        });
+    }
+    else {
+        $(".bird").finish().animate({
+            left: "-=" + moveDist
+        });
+    }
 };
 
 kd.RIGHT.down = function () {
-    $(".bird").finish().animate({
-        left: "+=9"
-    });
+    var $bird = $("#bird");
+    var birdLeft = $bird.position().left;
+    var birdWidth = $bird.width();
+    var birdRight = birdLeft + birdWidth;
+
+    var $gameBox = $("#gameBox");
+    var gameBoxLeft = $gameBox.position().left;
+    var gameBoxWidth = $gameBox.width();
+    var gameBoxRight = gameBoxLeft + gameBoxWidth;
+    var distance = gameBoxRight - birdRight;
+
+    if (distance < moveDist) {
+        $(".bird").finish().animate({
+            left: "+=" + distance
+        });
+    }
+    else {
+        $(".bird").finish().animate({
+            left: "+=" + moveDist
+        });
+    }
 };
 
 kd.run(function () {
